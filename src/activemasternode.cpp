ff3766bcdcef746b263052d4494ddc5e810778c8
PentaNode <pentanodecrypto@gmail.com>
2018-05-14 08:47:16 +0200

    Typo fix and cleanup
    Typo fix and cleanup

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -9,7 +9,7 @@
 #include "clientversion.h"
 
 //
-// Bootup the masternode, look for a 500 PTN input and register on the network
+// Bootup the masternode, look for a PTN input and register on the network
 //
 void CActiveMasternode::ManageStatus()
 {
